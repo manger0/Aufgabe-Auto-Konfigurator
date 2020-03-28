02e22a154160112d925b435ecc2a83030a5e4943
manger0 <mangerer1994@gmail.com>
2020-03-28 15:33:47 +0100

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {git
 
         // Variables
         ArrayList<Car> cars = new ArrayList<>();
