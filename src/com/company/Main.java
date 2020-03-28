7813dcbf7ccf449a989d7d849b523aa5ba5b0c2c
manger0 <mangerer1994@gmail.com>
2020-03-28 15:35:51 +0100

    tippo deleted

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {git
+    public static void main(String[] args) {
 
         // Variables
         ArrayList<Car> cars = new ArrayList<>();
